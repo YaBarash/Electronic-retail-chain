c2dd1aef185eed34593563c11c6cdd9eda71dbed
Alina Baranova <alina_nemo@mail.ru>
2024-12-26 15:17:53 +0300

    Add method of prohibition to create model company

--- retail_chain/views.py
+++ retail_chain/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, filters
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework import viewsets, filters, status
 from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 
 from retail_chain.models import Company
@@ -25,3 +27,20 @@ class CompanyViewSet(viewsets.ModelViewSet):
         if self.action:
             self.permission_classes = (IsAdminUser,)
         return super().get_permissions()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if (
+            request.data.get("debt") is None
+            and request.data.get("debt_currency") is None
+        ):
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        data = {"error": f"Ошибка с кодом 403. Вы не можете указывать задолженность"}
+        return JsonResponse(
+            data["error"],
+            safe=False,
+            status=status.HTTP_400_BAD_REQUEST,
+            json_dumps_params={"ensure_ascii": False},
+        )
